6126e2225b5c4a302474a1eb6c856dfed953f4e9
Sergi Vos <sergivb01@gmail.com>
2022-04-20 23:53:12 +0200

    partida5

--- main.hs
+++ main.hs
@@ -1,5 +1,9 @@
 import Butihask
 
+type LlistaCartes = [[Carta]]
+type Partida = [Carta]
+
+cartes1::LlistaCartes
 cartes1 = [
   [Carta Manilla Bastos, Carta Vuit Bastos, Carta Tres Espases, Carta As Copes, Carta Quatre Bastos, Carta Cavall Espases, Carta Set Copes, Carta As Oros, Carta Cinc Bastos, Carta Sota Copes, Carta Quatre Espases, Carta Set Bastos],
   [Carta Sota Bastos, Carta Cavall Bastos, Carta Manilla Espases, Carta Vuit Copes, Carta Cinc Oros, Carta Vuit Espases, Carta Manilla Copes, Carta Sis Oros, Carta Sota Oros, Carta Cinc Copes, Carta Set Oros, Carta Quatre Copes],
@@ -7,7 +11,7 @@ cartes1 = [
   [Carta Dos Bastos, Carta Tres Bastos, Carta Dos Espases, Carta Dos Copes, Carta Sis Bastos, Carta Set Espases, Carta Tres Oros, Carta Quatre Oros, Carta Rei Bastos, Carta Cavall Oros, Carta Manilla Oros, Carta Sota Espases]
  ]
 
-
+partida1::Partida
 partida1 = [
   Carta Manilla Bastos, Carta Sota Bastos, Carta As Bastos, Carta Dos Bastos,
   Carta Vuit Bastos, Carta Cavall Bastos, Carta Dos Oros, Carta Tres Bastos,
@@ -23,7 +27,10 @@ partida1 = [
   Carta Sota Espases, Carta Set Bastos, Carta Quatre Copes, Carta Cinc Espases
  ]
 
+cartes2::LlistaCartes
 cartes2 = cartes1
+
+partida2::Partida
 partida2 = [
   Carta Manilla Bastos, Carta Sota Bastos, Carta As Bastos, Carta Dos Bastos,
   Carta Vuit Bastos, Carta Cavall Bastos, Carta Dos Oros, Carta Tres Bastos,
@@ -39,6 +46,7 @@ partida2 = [
   Carta Sota Espases, Carta Set Bastos, Carta Quatre Copes, Carta As Bastos
  ]
 
+cartes3::LlistaCartes
 cartes3 = [
   [Carta Manilla Bastos, Carta Vuit Bastos, Carta Tres Espases, Carta As Copes, Carta Quatre Bastos, Carta Cavall Espases, Carta Set Copes, Carta As Oros, Carta Cinc Bastos, Carta Sota Copes, Carta Quatre Espases, Carta Set Bastos],
   [Carta Sota Bastos, Carta Cavall Bastos, Carta Manilla Espases, Carta Vuit Copes, Carta Cinc Oros, Carta Vuit Espases, Carta Manilla Copes, Carta Sis Oros, Carta Sota Oros, Carta Cinc Copes, Carta Set Oros, Carta Quatre Copes],
@@ -46,7 +54,7 @@ cartes3 = [
   [Carta Dos Bastos, Carta Tres Bastos, Carta Dos Espases, Carta Dos Copes, Carta Sis Bastos, Carta Set Espases, Carta Tres Oros, Carta Quatre Oros, Carta Rei Bastos, Carta Cavall Oros, Carta Manilla Oros, Carta Sota Espases]
  ]
 
-
+partida3::Partida
 partida3 = [
   Carta Manilla Bastos, Carta Sota Bastos, Carta Cinc Espases, Carta Dos Bastos,
   Carta Vuit Bastos, Carta Cavall Bastos, Carta Dos Oros, Carta Tres Bastos,
@@ -62,10 +70,43 @@ partida3 = [
   Carta Sota Espases, Carta Set Bastos, Carta Quatre Copes, Carta Cinc Espases
  ]
 
-partides :: [([[Carta]], [Carta], Trumfu, String)]
+cartes4::LlistaCartes
+cartes4 = [
+  [Carta Manilla Bastos, Carta Vuit Bastos, Carta Tres Espases, Carta As Copes, Carta Quatre Bastos, Carta Cavall Espases, Carta Set Copes, Carta As Oros, Carta Cinc Bastos, Carta Sota Copes, Carta Quatre Espases, Carta Set Bastos],
+  [Carta Sota Bastos, Carta Cavall Bastos, Carta Manilla Espases, Carta Vuit Copes, Carta Cinc Oros, Carta Vuit Espases, Carta Manilla Copes, Carta Sis Oros, Carta Sota Oros, Carta Cinc Copes, Carta Set Oros, Carta Quatre Copes],
+  [Carta As Bastos, Carta Dos Oros, Carta Rei Espases, Carta Cavall Copes, Carta Vuit Oros, Carta As Espases, Carta Rei Copes, Carta Rei Oros, Carta Tres Copes, Carta Sis Copes, Carta Sis Espases, Carta Cinc Espases],
+  [Carta Dos Bastos, Carta Tres Bastos, Carta Dos Espases, Carta Dos Copes, Carta Sis Bastos, Carta Set Espases, Carta Tres Oros, Carta Quatre Oros, Carta Rei Bastos, Carta Cavall Oros, Carta Manilla Oros, Carta Sota Espases]
+ ]
+
+partida4::Partida
+partida4 = [
+  Carta Manilla Bastos, Carta Sota Bastos, Carta As Bastos, Carta Dos Bastos,
+  Carta Vuit Bastos, Carta Cavall Bastos, Carta Dos Oros, Carta Tres Bastos,
+  Carta Rei Espases, Carta Dos Espases, Carta Tres Espases, Carta Vuit Espases,
+  Carta Vuit Copes, Carta Cavall Copes, Carta Dos Copes, Carta As Copes,
+  Carta Quatre Bastos, Carta Cinc Oros, Carta Vuit Oros, Carta Sis Bastos,
+  Carta As Espases, Carta Set Espases, Carta Cavall Espases, Carta Vuit Espases,
+  Carta Rei Copes, Carta Tres Oros, Carta Set Copes, Carta Manilla Copes,
+  Carta Quatre Oros, Carta As Oros, Carta Sis Oros, Carta Rei Oros,
+  Carta Cinc Bastos, Carta Sota Oros, Carta Tres Copes, Carta Rei Bastos,
+  Carta Cinc Copes, Carta Sis Copes, Carta Cavall Oros, Carta Sota Copes,
+  Carta Manilla Oros, Carta Quatre Espases, Carta Set Oros, Carta Sis Espases,
+  Carta Sota Espases, Carta Set Bastos, Carta Quatre Copes, Carta Cinc Espases
+ ]
+
+cartes5::LlistaCartes
+cartes5 = cartes1
+
+partida5 :: Partida
+partida5 = partida1
+
+partides :: [(LlistaCartes, Partida, Trumfu, String)]
 partides = [
-  (cartes1, partida1, Trumfu Oros, "Partida exemple (resultat 37 35)"),
-  (cartes2, partida2, Trumfu Oros, "Jugador tira una carta que ja havia tirat")
+  (cartes1, partida1, Trumfu Oros, "Partida correcte d'exemple (resultat 37 35)"),
+  (cartes2, partida2, Trumfu Oros, "Jugador tira una carta que ja havia tirat"),
+  (cartes3, partida3, Trumfu Oros, "Jugador té cartes del pal de sortida però tira un altre pal"),
+  (cartes4, partida4, Trumfu Oros, "Jugador pot matar la carta que guanya (rivals) amb pal de sortida i no ho fa"),
+  (cartes5, partida5, Butifarra, "Jugador tira en un ordre incorrecte")
  ]
 
 size :: Int
--- partida1.hs
+++ partida1.hs
@@ -3,7 +3,7 @@
 -- % La puntuaci� per parelles �s 37 i 35.
 import Butihask
 
-cartes1 =[
+cartes1 = [
   [Carta Manilla Bastos, Carta Vuit Bastos, Carta Tres Espases, Carta As Copes, Carta Quatre Bastos, Carta Cavall Espases, Carta Set Copes, Carta As Oros, Carta Cinc Bastos, Carta Sota Copes, Carta Quatre Espases, Carta Set Bastos],
   [Carta Sota Bastos, Carta Cavall Bastos, Carta Manilla Espases, Carta Vuit Copes, Carta Cinc Oros, Carta Vuit Espases, Carta Manilla Copes, Carta Sis Oros, Carta Sota Oros, Carta Cinc Copes, Carta Set Oros, Carta Quatre Copes],
   [Carta As Bastos, Carta Dos Oros, Carta Rei Espases, Carta Cavall Copes, Carta Vuit Oros, Carta As Espases, Carta Rei Copes, Carta Rei Oros, Carta Tres Copes, Carta Sis Copes, Carta Sis Espases, Carta Cinc Espases],
@@ -12,30 +12,26 @@ cartes1 =[
 
 
 partida1 = [
-	 Carta Manilla Bastos, Carta Sota Bastos, Carta As Bastos, Carta Dos Bastos,
-	 Carta Vuit Bastos, Carta Cavall Bastos, Carta Dos Oros, Carta Tres Bastos,
-	 Carta Rei Espases, Carta Dos Espases, Carta Tres Espases, Carta Manilla Espases,
-	 Carta Vuit Copes, Carta Cavall Copes, Carta Dos Copes, Carta As Copes,
-	 Carta Quatre Bastos, Carta Cinc Oros, Carta Vuit Oros, Carta Sis Bastos,
-	 Carta As Espases, Carta Set Espases, Carta Cavall Espases, Carta Vuit Espases,
-	 Carta Rei Copes, Carta Tres Oros, Carta Set Copes, Carta Manilla Copes,
-	 Carta Quatre Oros, Carta As Oros, Carta Sis Oros, Carta Rei Oros,
-	 Carta Cinc Bastos, Carta Sota Oros, Carta Tres Copes, Carta Rei Bastos,
-	 Carta Cinc Copes, Carta Sis Copes, Carta Cavall Oros, Carta Sota Copes,
-	 Carta Manilla Oros, Carta Quatre Espases, Carta Set Oros, Carta Sis Espases,
-	 Carta Sota Espases, Carta Set Bastos, Carta Quatre Copes, Carta Cinc Espases
+  Carta Manilla Bastos, Carta Sota Bastos, Carta As Bastos, Carta Dos Bastos,
+  Carta Vuit Bastos, Carta Cavall Bastos, Carta Dos Oros, Carta Tres Bastos,
+  Carta Rei Espases, Carta Dos Espases, Carta Tres Espases, Carta Manilla Espases,
+  Carta Vuit Copes, Carta Cavall Copes, Carta Dos Copes, Carta As Copes,
+  Carta Quatre Bastos, Carta Cinc Oros, Carta Vuit Oros, Carta Sis Bastos,
+  Carta As Espases, Carta Set Espases, Carta Cavall Espases, Carta Vuit Espases,
+  Carta Rei Copes, Carta Tres Oros, Carta Set Copes, Carta Manilla Copes,
+  Carta Quatre Oros, Carta As Oros, Carta Sis Oros, Carta Rei Oros,
+  Carta Cinc Bastos, Carta Sota Oros, Carta Tres Copes, Carta Rei Bastos,
+  Carta Cinc Copes, Carta Sis Copes, Carta Cavall Oros, Carta Sota Copes,
+  Carta Manilla Oros, Carta Quatre Espases, Carta Set Oros, Carta Sis Espases,
+  Carta Sota Espases, Carta Set Bastos, Carta Quatre Copes, Carta Cinc Espases
  ]
 
 
 -- >>> puntsParelles cartes1 (Trumfu Oros) partida1 1
 -- Just (37,35)
 
--- >>> trampa cartes1 (Trumfu Oros) partida1 1
--- Nothing
-
--- cartesGuanyades:: Trumfu -> [Carta] -> Int -> ([Carta],[Carta])
--- >>> cartesGuanyades (Trumfu Oros) partida1 1
--- ([Manilla de Bastos,Sota de Bastos,As de Bastos,Dos de Bastos,Vuit de Bastos,Cavall de Bastos,Dos d'Oros,Tres de Bastos,Vuit de Copes,Cavall de Copes,Dos de Copes,As de Copes,Quatre de Bastos,Cinc d'Oros,Vuit d'Oros,Sis de Bastos,As d'Espases,Set d'Espases,Cavall d'Espases,Vuit d'Espases,Quatre d'Oros,As d'Oros,Sis d'Oros,Rei d'Oros],[Rei d'Espases,Dos d'Espases,Tres d'Espases,Manilla d'Espases,Rei de Copes,Tres d'Oros,Set de Copes,Manilla de Copes,Cinc de Bastos,Sota d'Oros,Tres de Copes,Rei de Bastos,Cinc de Copes,Sis de Copes,Cavall d'Oros,Sota de Copes,Manilla d'Oros,Quatre d'Espases,Set d'Oros,Sis d'Espases,Sota d'Espases,Set de Bastos,Quatre de Copes,Cinc d'Espases])
+-- >>> trampa cartes1 Butifarra partida1 1
+-- Just ([Rei d'Espases,Dos d'Espases,Tres d'Espases,Manilla d'Espases],3,2)
 
 -- >>> punts (fst(cartesGuanyades (Trumfu Oros) partida1 1))
 -- >>> length(fst(cartesGuanyades (Trumfu Oros) partida1 1)) `div` 4
